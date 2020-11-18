33c5b24977ed61baba01880eb80586d995c55d46
Dmitry <dmitry.malyhsevv@ya.ru>
2020-11-19 00:07:41 +0300

    New code to to-do bot.

---
+++ DB.py
@@ -0,0 +1,150 @@
+import sqlite3
+import secret
+
+# Показывает текущие задачи, надо сделать разветвление по user_id, чтобы в зависимости от id каждый видел свои задачи,
+# наверное проще решить это посредством корректировки структуры самой БД.
+def read_task(idu):
+    conn = sqlite3.connect(secret.DB)
+    cur = conn.cursor()
+
+    # Сначала нужно определить роль
+    # вах какой костыль
+    # получили вот такого очкарика  [0][0]
+    role = [x for x in cur.execute('SELECT Role FROM Users WHERE Id_Users = {}'.format(idu))][0][0]
+
+    # Выводим все задачи - кажется жестко(например есди задач 100)
+    if role == 'Руководитель':
+        result = [x for x in cur.execute('SELECT * FROM Task')]
+        text = '22'
+        # Чуть чуть переделал
+        # Пока не меняет id пользователей
+        response_data = []
+        for i in result:
+            temp = []
+            temp.append('Номер задачи: {}\n'.format(i[0]))
+            temp.append('Назавание задачи: {}\n'.format(i[1]))
+            temp.append('Выполнение: {}\n'.format(i[2]))
+            result = [x for x in cur.execute('SELECT User_Name FROM Users WHERE Id_Users = {}'.format(i[3]))]
+            if result:
+                name = result[0][0]
+            else:
+                name = "конь в пальто"
+            temp.append('Исполнитель: {}\n'.format(name))
+            temp.append('Коментарии:{}\n'.format(i[4]))
+
+            response_data.append(' '.join(temp))
+        # поставил пробел , чтобы tg думал что строка непустая
+        text = ' \n'.join(response_data)
+
+
+    elif role == 'Ведущий':
+        result = [x for x in cur.execute('SELECT * FROM Task t JOIN Users u ON t. ')]
+        print(result)
+        string = 0
+        text = ''
+        for i in range(len(result)):
+            result_print = 'Номер задачи: {}\nНазавание задачи: {}\nВыполнение: {}\n Исполнитель: {}\n Коментарии:{}\n'. \
+                format(result[string][0], result[string][1], result[string][2], result[string][3], result[string][4])
+            text = text + result_print
+            string += 1
+
+    elif role == 'Инженер':
+        result = [x for x in cur.execute('SELECT * FROM Task')]
+        string = 0
+        text = ''
+        for i in range(len(result)):
+            result_print = 'Номер задачи: {}\nНазавание задачи: {}\nВыполнение: {}\n Исполнитель: {}\n Коментарии:{}\n'. \
+                format(result[string][0], result[string][1], result[string][2], result[string][3], result[string][4])
+            text = text + result_print
+            string += 1
+    else:
+        text = 'Роль не определена'
+
+    return text
+
+
+# Запись задачи.
+def write_task(msg):
+    conn = sqlite3.connect('FGMG.db')
+    cur = conn.cursor()
+    db_data = [(msg,)]
+    result = cur.executemany("""INSERT INTO Task(Task_Name) VALUES(?)""", db_data)
+    conn.commit()
+
+
+# Запись комента
+def write_comment(msg):
+    conn = sqlite3.connect('FGMG.db')
+    cur = conn.cursor()
+    db_data = [(msg,)]
+    cur.executemany("""INSERT INTO Comments(Comment) VALUES(?) """, db_data)
+    conn.commit()
+
+
+# Возвращает Фамилию юзера по id
+def id_filter(idu):
+    conn = sqlite3.connect('FGMG.db')
+    cur = conn.cursor()
+    result = [x for x in cur.execute("SELECT User_Name FROM Users WHERE Id_Users = {} ".format(idu))]
+    return result
+
+
+# Записывает юзера, Фамлию, Роль, id
+def write_user(user):
+    conn = sqlite3.connect('FGMG.db')
+    cur = conn.cursor()
+    cur.executemany("""INSERT INTO Users(User_Name, Role, Id_Users) VALUES(?, ?, ?) """, user)
+    conn.commit()
+
+
+# Читает роль по id.
+def read_role(idu):
+    conn = sqlite3.connect('FGMG.db')
+    cur = conn.cursor()
+    result = [x for x in cur.execute("""SELECT Role FROM Users WHERE Id_Users = {} """.format(idu))]
+    conn.commit()
+    return result[0][0]
+
+
+# Возвращает строку с фамилиями ведущих из ТГ.
+def list_users_mid():
+    conn = sqlite3.connect('FGMG.db')
+    cur = conn.cursor()
+    result = [x for x in cur.execute("""SELECT User_Name FROM Users WHERE Role = 'Ведущий' """)]
+    string = 0
+    text = ''
+    for i in range(len(result)):
+        result_print = '{}\n'.format(result[string][0])
+        text = text + result_print
+        string += 1
+    return text
+
+
+# Возвращает строку с фамилиями инженеров.
+def list_users_top():
+    conn = sqlite3.connect('FGMG.db')
+    cur = conn.cursor()
+    result = [x for x in cur.execute("""SELECT User_Name FROM Users WHERE Role = 'Инженер' """)]
+    string = 0
+    text = ''
+    for i in range(len(result)):
+        result_print = '{}\n'.format(result[string][0])
+        text = text + result_print
+        string += 1
+    return text
+
+
+# Принимает имя исполнителя, записывает в последнюю задачу.
+def write_worker_task(user_name):
+    conn = sqlite3.connect('FGMG.db')
+    cur = conn.cursor()
+    all = [x for x in cur.execute("""SELECT * FROM Task""")]
+    index_end_task = len(all) - 1
+    end = [x for x in cur.execute("""SELECT No_task FROM Task""")]
+    end_no_task = end[index_end_task][0]
+    id_bd = [x for x in conn.execute("""SELECT Id_Users FROM Users WHERE User_Name = '{}'""".format(user_name))]
+    id_worker = id_bd[0][0]
+    result = cur.execute("""UPDATE Task SET id_worker = '{}'  WHERE No_task = {}  """.format(id_worker, end_no_task))
+    conn.commit()
+
+
---
+++ TG TODO.py
@@ -0,0 +1,153 @@
+import telebot
+import DB
+import secret
+
+# token tg
+bot = telebot.TeleBot(secret.TOKEN)
+Keyboard_cup = telebot.types.ReplyKeyboardMarkup(True, False)
+Keyboard_mid = telebot.types.ReplyKeyboardMarkup(True, False)
+Keyboard_top = telebot.types.ReplyKeyboardMarkup(True, False)
+Keyboard_choose = telebot.types.ReplyKeyboardMarkup(True, True)
+Keyboard_start = telebot.types.ReplyKeyboardMarkup(True, True)
+Keyboard_users = telebot.types.ReplyKeyboardMarkup(True, True)
+button_next = telebot.types.ReplyKeyboardMarkup(True, True)
+Keyboard_cup.row('Cоздать задачу', 'Текущие задачи', 'Завершенные задачи')
+Keyboard_mid.row('Просмотр задач', 'Переназначение задач', 'Завершение задания')
+Keyboard_top.row('Просмотр задач', 'Завершение задачи')
+Keyboard_choose.row('Руководитель', 'Ведущий', 'Инженер')
+Keyboard_start.row('Начать')
+button_next.row('Продолжить')
+
+
+# This handler for command start, before writed command scanning the id user's, if id have in database, bot send hi-message,
+# else asks write the user role and write user info in database.
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    if DB.id_filter(message.from_user.id) == []:
+        msg = bot.send_message(message.chat.id,
+                               'Добро пожаловать,' + message.from_user.first_name + ', выберите вашу должность:',
+                               reply_markup=Keyboard_choose)
+        bot.register_next_step_handler(msg, write_user)
+    else:
+        msg = bot.send_message(message.chat.id,
+                               'Добро пожаловать,' + message.from_user.first_name + ', нажмите на кнопку,чтобы начать:',
+                               reply_markup=Keyboard_start)
+        bot.register_next_step_handler(msg, user_filter)
+
+
+def write_user(message):
+    user = [(message.from_user.last_name, message.text, message.from_user.id)]
+    DB.write_user(user)
+    msg = bot.send_message(message.chat.id, 'Вы успешно добавлены! Нажмите кнопку, чтобы начать:',
+                           reply_markup=Keyboard_start)
+    bot.register_next_step_handler(msg, user_filter)
+
+
+@bot.message_handler(content_types=['text'])
+# После определения роли, отправляем на ветку, читая роль.
+def user_filter(message):
+    """Определяет роль пользователя и отправляет на ветку пользователя"""
+    mci = message.chat.id
+    # print(message)
+
+    if DB.read_role(message.from_user.id) == 'Руководитель':
+        msg = bot.send_message(mci, 'Выберете вариант:', reply_markup=Keyboard_cup)
+        bot.register_next_step_handler(msg, button_read)
+    elif DB.read_role(message.from_user.id) == 'Ведущий':
+        msg = bot.send_message(mci, 'Выберете вариант:', reply_markup=Keyboard_mid)
+        bot.register_next_step_handler(msg, button_read)
+    elif DB.read_role(message.from_user.id) == 'Инженер':
+        msg = bot.send_message(mci, 'Выберете вариант:', reply_markup=Keyboard_top)
+        bot.register_next_step_handler(msg, button_read)
+
+
+# Функция чтения кнопок, отправляет на функцию выполнения действия в зависимости от выбранной кнопки.
+# Если кнопка еще не разработана, возвращет на себя.
+def button_read(message):
+    mci = message.chat.id
+    mt = message.text
+    if mt == 'Текущие задачи':
+        msg = bot.send_message(mci, 'Вот список текущих задач:')
+        bot.send_message(mci, f"{DB.read_task(mci)}")
+        bot.register_next_step_handler(msg, button_read)
+    elif mt == 'Cоздать задачу':
+        msg = bot.send_message(mci, 'Напишите вашу задачу:')
+        bot.register_next_step_handler(msg, write_task)
+    elif mt == 'Завершенные задачи':
+        msg = bot.send_message(mci, 'Список завершенных задач:')
+        bot.register_next_step_handler(msg, button_read)
+    elif mt == 'Просмотр задач':
+        mci = message.chat.id
+        msg = bot.send_message(mci, f'Вот список текущих задач:\n {DB.read_task(mci)}')
+        bot.register_next_step_handler(msg, button_read)
+
+    elif mt == 'Переназначение задач':
+        msg = bot.send_message(mci, 'Выберете, на кого переназначить:')
+        bot.register_next_step_handler(msg, reget_task)
+    elif mt == 'Завершение задания':
+        msg = bot.send_message(mci, 'Выберете, какую задачу вы хотите завершить:')
+        bot.register_next_step_handler(msg, end_task)
+
+
+# Функция создания задачи, есть только у РУКОВОДИТЕЛЯ, записывает название задачи и отправляет на комент.
+def write_task(message):
+    mci = message.chat.id
+    text = message.text
+    DB.write_task(text)
+    msg = bot.send_message(mci, 'Введите комментарий:')
+    bot.register_next_step_handler(msg, write_comment_сup())
+
+
+# Запись комента, записывает коментарий и выводит список пользователей(руководителю - ведущие, ведущим - инженеры ),
+# которым можно назначить задачу, перенаправляет на запись пользователя, НЕ ВЫПОЛНЕНО!!!!                       !!!!
+def write_comment_сup(message):
+    mci = message.chat.id
+    text = message.text
+    DB.write_comment(text)
+    msg = bot.send_message(mci, 'Комментарий добавлен! Напишите исполнителя из списка:\n\n' + DB.list_users_mid())
+    bot.register_next_step_handler(msg, write_user_for_task)
+
+
+# Записывает назначеного пользователя на задачу, направляет на кнопку, возвращающую на начало ветки.
+def write_user_for_task(message):
+    mci = message.chat.id
+    text = message.text
+    DB.write_worker_task(text)
+    msg = bot.send_message(mci, 'Исполнитель выбран! Чтобы продолжить, нажмите на кнопку:', reply_markup=button_next)
+    bot.register_next_step_handler(msg, button_read)
+
+
+# Функция выводит список текущих задач, НЕ УЧИТЫВАЕТ РОЛЬ ПОЛЬЗОВАТЕЛЯ!!!!!                                      !!!
+def now_task(message):
+    id_user = message.from_user.id
+    mci = message.chat.id
+    msg = bot.send_message(mci, 'Вот список текущих задач:')
+    bot.send_message(mci, f"{DB.read_task(id_user)}")
+    bot.register_next_step_handler(msg, button_read)
+
+
+# Перенаправление задачи для ведущих, НЕ ВЫПОЛНЕНО ПЕРЕНАПРАВЛЕНИЕ!!!!!                                          !!!
+def reget_task(message):
+    mci = message.chat.id
+    mt = message.text
+    msg = bot.send_message(mci, 'Задача перенаправлена!')
+    bot.register_next_step_handler(msg, user_filter)
+
+
+# Завершение задачи для ВЕДУЩИХ И ИНЖЕНЕРОВ, НЕ ВЫПОЛНЕНО !!!                                                    !!!
+def end_task(message):
+    mci = message.chat.id
+    msg = bot.send_message(mci, 'Задача завершена!')
+    bot.register_next_step_handler(msg, user_filter)
+
+
+# При ошибке подключения должен переподключаться! Уточнить верность.
+if __name__ == '__main__':
+    while True:
+        try:
+            bot.polling(True)
+        except ConnectionError:
+            print('Подключение отвалилось! Переподключаем!')
+            bot.polling(True)
+
